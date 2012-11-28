e0ae51beb8dc93e4c1a0cdd792d6565b8bd4a1c8
Pawel Widomski <widomski.pawel@gmail.com>
2012-11-28 08:44:44 +0100

    BOOL type and NULL pointer

---
+++ common/types.h
@@ -0,0 +1,21 @@
+/*
+ * types.h
+ *
+ *  Created on: Nov 27, 2012
+ *      Author: widmo
+ */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef TYPES_H_
+#define TYPES_H_
+
+/* Includes ------------------------------------------------------------------*/
+
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+typedef enum {FALSE = 0, TRUE = 1} BOOL;
+#define NULL ((void*) 0)
+
+
+#endif /* TYPES_H_ */
